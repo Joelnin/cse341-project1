3b030ab6f00e3f81def12ff9bb50e89479726a93
Joelnin <joelninr@gmail.com>
2025-10-31 16:18:26 -0400

    Adding the Get and GetAll endpoints

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+
+const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -6,7 +8,12 @@ const port = process.env.PORT || 3000;
 app.use('/', require('./routes'))
 
 
+mongodb.initDB((err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port, () => (console.log(`Database is listening and node Running on port ${port}`)));
+    }
+})
 
-
-
-app.listen(port, () => (console.log(`Running on port ${port}`)));
